7ca84879d6f087c7b28a2e31f0543c90663d522e
kbpark <pkbnise@gmail.com>
2015-06-11 20:13:34 +0900

    getWasherFromServer added
    getWasherFromServer (Server means orbd) added.
    IN setup() and  getWasherSubscriberNumber(), getWasherFromServer() is
    called for getting service registered in orbd.

--- src/main/java/kr/ac/ajou/lazybones/components/WasherManager.java
+++ src/main/java/kr/ac/ajou/lazybones/components/WasherManager.java
@@ -1,7 +1,7 @@
 package kr.ac.ajou.lazybones.components;
 
 import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
@@ -12,11 +12,13 @@ import javax.annotation.PostConstruct;
 import kr.ac.ajou.lazybones.washerapp.Washer.Reservation;
 import kr.ac.ajou.lazybones.washerapp.Washer.ReservationQueue;
 import kr.ac.ajou.lazybones.washerapp.Washer.ReservationQueueHelper;
-import kr.ac.ajou.lazybones.washerapp.Washer.Washer;
-import kr.ac.ajou.lazybones.washerapp.Washer.WasherHelper;
 
 import org.omg.CORBA.ORB;
 import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.CosNaming.Binding;
+import org.omg.CosNaming.BindingIteratorHolder;
+import org.omg.CosNaming.BindingListHolder;
+import org.omg.CosNaming.BindingType;
 import org.omg.CosNaming.NamingContextExt;
 import org.omg.CosNaming.NamingContextExtHelper;
 import org.omg.CosNaming.NamingContextPackage.CannotProceed;
@@ -29,9 +31,10 @@ import org.springframework.stereotype.Component;
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class WasherManager {
 	private final String port = "1050";
-	private final String host = "localhost";
+	private final String host = "210.107.197.213";
 
 	private Map<String, ReservationQueue> washerReservationQueues;
+	ReservationQueue reservationQueue;
 
 	private NamingContextExt ncRef;
 
@@ -39,24 +42,71 @@ public class WasherManager {
 	// It will be used for adding washers.
 	@PostConstruct
 	public void setup() throws InvalidName {
+		// HashMap of <WasherName, ReservationQueue>
+		washerReservationQueues = new HashMap<String, ReservationQueue>();
+
 		// properties for ORB. You can change hostname and port here.
-		washerReservationQueues = new HashMap<>();
-
 		Properties props = new Properties();
-		props.put("org.omg.CORBA.ORBInitialPort", this.port);
-		props.put("org.omg.CORBA.ORBInitialHost", this.host);
 
-		String[] emptyArgs = new String[2];
-		emptyArgs[0] = "Welcome to the";
-		emptyArgs[1] = "Jungle";
+		try {
 
-		// STEP 1: create and initialize the ORB
-		ORB orb = ORB.init(emptyArgs, props);
+			props.put("org.omg.CORBA.ORBInitialPort", this.port);
+			props.put("org.omg.CORBA.ORBInitialHost", this.host);
 
-		// STEP 2: get the root naming context (naming service)
-		org.omg.CORBA.Object objRef = orb
-				.resolve_initial_references("NameService");
-		ncRef = NamingContextExtHelper.narrow(objRef);
+			String[] emptyArgs = new String[2];
+			emptyArgs[0] = "Welcome to the";
+			emptyArgs[1] = "Jungle";
+
+			// STEP 1: create and initialize the ORB
+			ORB orb = ORB.init(emptyArgs, props);
+
+			// STEP 2: get the root naming context (naming service)
+			org.omg.CORBA.Object objRef = orb
+					.resolve_initial_references("NameService");
+			ncRef = NamingContextExtHelper.narrow(objRef);
+			
+			getWasherFromServer();
+
+			/*
+			 * Get registered washers from orbd and insert into
+			 * washerReservationQueues
+			 * 
+			 * reference:
+			 * http://www.massapi.com/class/org/omg/CosNaming/BindingListHolder
+			 * .html
+			 * http://www.informit.com/articles/article.aspx?p=23266&seqNum=9
+			 */
+
+			/*
+			 * For getting list of registered washers in from orbd
+			 */
+			/*
+			 * BindingListHolder bl = new BindingListHolder();
+			 * BindingIteratorHolder blIt = new BindingIteratorHolder();
+			 * ncRef.list(1000, bl, blIt); Binding bindings[] = bl.value;
+			 * ReservationQueue reservationQueue;
+			 * 
+			 * // Store every registered washer and its queue for (int i = 0; i
+			 * < bindings.length; i++) { int lastIx =
+			 * bindings[i].binding_name.length - 1; // check to see if this is a
+			 * naming context with Object type if (bindings[i].binding_type ==
+			 * BindingType.nobject) { String washerName =
+			 * bindings[i].binding_name[lastIx].id; reservationQueue =
+			 * ReservationQueueHelper.narrow(ncRef .resolve_str(washerName));
+			 * washerReservationQueues.put(washerName, reservationQueue); } }
+			 * 
+			 * // Print washer list for test Iterator<String> iterator =
+			 * washerReservationQueues.keySet() .iterator(); while
+			 * (iterator.hasNext()) { String key = iterator.next();
+			 * System.out.println("key=" + key); Reservation[] reservations =
+			 * washerReservationQueues.get(key).reservations(); for(Reservation
+			 * reservation : reservations) { System.out.println("who: " +
+			 * reservation.who + ", duration: " + reservation.duration); } }
+			 */
+		} catch (Exception e) {
+			System.out.println("ERROR : " + e);
+			e.printStackTrace(System.out);
+		}
 	}
 
 	public boolean addWasher(String name) {
@@ -70,7 +120,7 @@ public class WasherManager {
 				return false;
 		} catch (NotFound e) {
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			// e.printStackTrace();
 			System.out.println("Not found in the Naming Context: " + name);
 		} catch (CannotProceed e) {
 			// TODO Auto-generated catch block
@@ -97,7 +147,8 @@ public class WasherManager {
 
 	public Map<String, ReservationQueue> getReservationQueues() {
 		Map<String, ReservationQueue> map = new HashMap<>();
-		for (Entry<String, ReservationQueue> item : washerReservationQueues.entrySet()) {
+		for (Entry<String, ReservationQueue> item : washerReservationQueues
+				.entrySet()) {
 			if (!item.getValue()._non_existent())
 				map.put(item.getKey(), item.getValue());
 			else
@@ -106,10 +157,69 @@ public class WasherManager {
 
 		return map;
 	}
-	
-	public Map<String, Integer> getWasherSubscriberNumbers(){
+
+	public boolean getWasherFromServer() {
+		try {
+			/*
+			 * Get registered washers from orbd and insert into
+			 * washerReservationQueues
+			 * 
+			 * reference:
+			 * http://www.massapi.com/class/org/omg/CosNaming/BindingListHolder
+			 * .html
+			 * http://www.informit.com/articles/article.aspx?p=23266&seqNum=9
+			 */
+
+			/*
+			 * For getting list of registered washers in from orbd
+			 */
+
+			BindingListHolder bl = new BindingListHolder();
+			BindingIteratorHolder blIt = new BindingIteratorHolder();
+			ncRef.list(1000, bl, blIt);
+			Binding bindings[] = bl.value;
+			ReservationQueue reservationQueue;
+
+			// Store every registered washer and its queue
+			for (int i = 0; i < bindings.length; i++) {
+				int lastIx = bindings[i].binding_name.length - 1;
+				// check to see if this is a naming context with Object type
+				if (bindings[i].binding_type == BindingType.nobject) {
+					String washerName = bindings[i].binding_name[lastIx].id;
+					reservationQueue = ReservationQueueHelper.narrow(ncRef
+							.resolve_str(washerName));
+					washerReservationQueues.put(washerName, reservationQueue);
+				}
+			}
+
+			// Print washer list for test
+			Iterator<String> iterator = washerReservationQueues.keySet()
+					.iterator();
+			while (iterator.hasNext()) {
+				String key = iterator.next();
+				System.out.println("key=" + key);
+				Reservation[] reservations = washerReservationQueues.get(key)
+						.reservations();
+				for (Reservation reservation : reservations) {
+					System.out.println("who: " + reservation.who
+							+ ", duration: " + reservation.duration);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
+
+	}
+
+	public Map<String, Integer> getWasherSubscriberNumbers() {
+		getWasherFromServer();
+		
 		Map<String, Integer> map = new HashMap<>();
-		for (Entry<String, ReservationQueue> item : washerReservationQueues.entrySet()) {
+		for (Entry<String, ReservationQueue> item : washerReservationQueues
+				.entrySet()) {
 			if (!item.getValue()._non_existent())
 				map.put(item.getKey(), item.getValue().size());
 			else
@@ -118,27 +228,27 @@ public class WasherManager {
 		System.out.println(map);
 
 		return map;
-		
+
 	}
-	
-	public Set<String> getReservationQueueNames(){
+
+	public Set<String> getReservationQueueNames() {
 		return washerReservationQueues.keySet();
 	}
-	
-	public ReservationQueue getReservationQueue(String name){
+
+	public ReservationQueue getReservationQueue(String name) {
 		return washerReservationQueues.get(name);
 	}
 
 	public void setWashers(Map<String, ReservationQueue> washerReservationQueues) {
 		this.washerReservationQueues = washerReservationQueues;
 	}
-	
-	public void enqueue(String washerName, String who, long duration){
+
+	public void enqueue(String washerName, String who, long duration) {
 		washerReservationQueues.get("name").enqueue(who, duration);
 	}
-	
-	public void remove(String washerName, int index){
+
+	public void remove(String washerName, int index) {
 		washerReservationQueues.get("name").remove(index);
 	}
-	
+
 }
