759dd9fb963b87c17caadf142e6af02b452c0303
jcdby <jcdby@Ethan>
2015-06-13 12:54:16 +0900

    Implementate the function that can register Service Provider when it initiate.

--- src/main/java/kr/ac/ajou/lazybones/restcontrollers/ReservationRESTController.java
+++ src/main/java/kr/ac/ajou/lazybones/restcontrollers/ReservationRESTController.java
@@ -8,20 +8,24 @@ import org.springframework.web.client.RestTemplate;
 
 
 @Component
+//This Component will send the information about this Service Provider when SP server initiated.
 public class ReservationRESTController {
 	
-	private final String regAddress = "http://210.107.197.44:8080/"
+	private String regAddress = "http://210.107.197.150:8080/"//PA(Personal Assisstant) server address
 			+ "PAmanager/webresources/RegisterServiceProvider/{SPname}/{URI}";//The Personal Assisstant system server address.
 	
 	private final String SPname = "WasherMan";//the name for registration
-	private final String Uri = "";//the name for retrieving the services in this Service Provider.
+	private final String Uri = "www.naver.com";//the name for retrieving the services in this Service Provider.
+	//uri should be where PA can get services from uri.
 	
 	private RestTemplate restTempleate = new RestTemplate();
 	
 	@PostConstruct
 	public void RegisterServiceProvider(){
 		
-		restTempleate.getForObject(regAddress, void.class, SPname, Uri);
+		//Register Service Provider when initiation.(Include SP name, and the place where can get the services from uri.)
+		String result = restTempleate.getForObject(regAddress, String.class, SPname, Uri);
+		
 		
 	}
 		
