64a984fefe864ea358de958da34fa8ebbd0a8b75
HDHan <bab6ting@gmail.com>
2015-06-14 15:36:11 +0900

    Minor changes (Duration of reservation)

--- src/main/java/kr/ac/ajou/lazybones/restcontrollers/ReservationRESTController.java
+++ src/main/java/kr/ac/ajou/lazybones/restcontrollers/ReservationRESTController.java
@@ -22,7 +22,7 @@ import org.springframework.web.client.RestTemplate;
 //This Component will send the information about this Service Provider when SP server initiated.
 public class ReservationRESTController {
 	
-	private String regAddress = "http://210.107.197.150:8080/"//PA(Personal Assisstant) server address
+	private String regAddress = "http://210.107.197.44:8080/"//PA(Personal Assisstant) server address
 			+ "PAmanager/webresources/RegisterServiceProvider/{SPname}/{URI}";//The Personal Assisstant system server address.
 	
 	private final String SPname = "WasherMan";//the name for registration
@@ -31,14 +31,14 @@ public class ReservationRESTController {
 	
 	private RestTemplate restTempleate = new RestTemplate();
 	
-	@PostConstruct
-	public void RegisterServiceProvider(){
-		
-		//Register Service Provider when initiation.(Include SP name, and the place where can get the services from uri.)
-		String result = restTempleate.getForObject(regAddress, String.class, SPname, Uri);
-			
-	}
-	
+//	@PostConstruct
+//	public void RegisterServiceProvider(){
+//		
+//		//Register Service Provider when initiation.(Include SP name, and the place where can get the services from uri.)
+//		String result = restTempleate.getForObject(regAddress, String.class, SPname, Uri);
+//			
+//	}
+//	
 	@RequestMapping(value = "/GetServicesRegisterEntry/{SPid}", method = RequestMethod.GET,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<ServiceForPA> getServices(@PathVariable("SPid") Long id)
