bfa5a0389b61941605a2a53fa5bfb8190d9b5db6
Jincheng <jcdby520@gmail.com>
2015-06-15 14:04:27 +0800

    Successfully be able to connect 3 systems by RESTful

--- src/main/java/kr/ac/ajou/lazybones/restcontrollers/ReservationRESTController.java
+++ src/main/java/kr/ac/ajou/lazybones/restcontrollers/ReservationRESTController.java
@@ -3,6 +3,7 @@ package kr.ac.ajou.lazybones.restcontrollers;
 
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,13 +12,10 @@ import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
-
-import kr.ac.ajou.lazybones.DataStructure.ServiceForPA;
-import kr.ac.ajou.lazybones.DataStructure.ServiceProviderForPA;
-
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,55 +23,45 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import com.google.gson.Gson;
+
 
 @Component
 //This Component will send the information about this Service Provider when SP server initiated.
 public class ReservationRESTController {
 	
-	//private String getregAddress = "http://localhost:8080/"//PA(Personal Assisstant) server address
-			//+ "PAmanager/webresources/RegisterServiceProvider/{SPname}/{URI}";//The Personal Assisstant system server address.
-	//post uri
-	private String postregAddress = "http://localhost:8080/PAmanager/webresources/RegisterServiceProviderPost";
+	private String getregAddress = "http://localhost:8080/"//PA(Personal Assisstant) server address
+			+ "PAmanager/webresources/RegisterServiceProvider/{SPname}/{ServiceName}/{RedirectionAddress}";//The Personal Assisstant system server address.
 	
-	private final String SPname = "WasherManPost";//the name for registration
-	private final String Uri = "GetServicesRegisterEntry";//the name for retrieving the services in this Service Provider.
+	//the name for registration
+	private final String SPname = "WasherMan";
+	//the name for retrieving the services in this Service Provider.
 	//uri should be where PA can get services from uri.
+	private final String ServiceEntryUri = "localhost%3A8090%2FWasherMan%2FGetServicesRegisterEntry";
 	
-	//private Map<String, String> map;
-	
+		
 	
 	private RestTemplate restTempleate = new RestTemplate();
 	
 	@PostConstruct
+	//This method is used for registering it to PA System.
 	public void RegisterServiceProvider(){
 		
-		
-		//map = new HashMap<String, String>();
-		//map.put("SPname", "WasherManPost");
-		//map.put("Uri","GetServicesRegisterEntry1" );
 		//Register Service Provider when initiation.
 		//(Include SP name, and the place where can get the services from uri.)
-		//String result = restTempleate.getForObject(getregAddress, String.class, map);
+		System.out.println("I am in!");
+		String result = restTempleate.getForObject(getregAddress, String.class, SPname, "Reservation", ServiceEntryUri);
 		
+		System.out.println(result);
 		
-		//Post method
-		ServiceProviderForPA sp = new ServiceProviderForPA(SPname, Uri);
-		URI result1 = restTempleate.postForLocation(postregAddress, sp);
-		System.out.println(result1);
-		
+		if(!result.equals("OK"))
+		{
+			System.out.println("Registeration is failed");
+		}
 		
 	}
 	
-	/*@RequestMapping(value = "/GetServicesRegisterEntry/{SPid}", method = RequestMethod.GET,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<ServiceForPA> getServices(@PathVariable("SPid") Long id)
-	{
 		
-		
-		return null;
-		
-	}*/
-	
 		
 	
 }
